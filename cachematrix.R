b9cc4c297282b464900c515318472831dfb38220
Pantheia <PantheiaX@gmail.com>
2019-07-21 21:40:14 -0500

    update of function descriptions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## _makeCacheMatrix_ creates an instance of the object with four functions:
+## set, get, setinv, getinv
+## and also saves those functions to a list so they can be called with "object%set" etc
+## _cacheSolve_ uses the solve() function on the input, it can be applied
+## to an instance of the makeCacheMatrix object
 
-## Write a short comment describing this function
+## make an instance of a makeCacheMatrix object:
+## m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2) #define a matrix
+## myMatrix_object <- makeCacheMatrix(m1)
+## after initialization, a new matrix can bet set by:
+## n2 <- matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2, ncol = 2)
+## myMatrix_object$set(n2)
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -23,18 +31,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## computes the solve() of the input object, unless
+## that value has already been calculated, in which
+## case that inverse is pulled from memory
+## cacheSolve(myMatrix_object)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inv <- x$getinv()
-        if (!is.null(inv)  &  x == x$get()) {
+        V <- x$getinv()
+        if (!is.null(V) ) {
                 
                 message("getting cached data")
-                return(inv)
+                return(V)
         }
         data <- x$get()
-        inv <- solve(x, ...)
-        inv
+        V <- solve(data, ...)
+        x$setinv(V)
+        V
         
 }
